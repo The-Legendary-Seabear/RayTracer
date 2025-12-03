d4d85b0c7fa02c7c8d10f2ba1c9d4d2c8c9bcc6e
The-Legendary-Seabear <akohn@student.neumont.edu>
2025-12-02 22:20:27 -0700

    [E] Ray Tracer - Ray

---
+++ Source/Ray.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <glm/glm.hpp>
+
+struct Ray {
+public:
+	
+	Ray() = default;
+	Ray(const glm::vec3& origin, const glm::vec3& direction) : origin(origin), direction(direction) {}
+
+	glm::vec3 at(float t) const {
+		return origin + t * direction;
+	}
+
+
+	glm::vec3 origin;
+	glm::vec3 direction;
+
+
+
+};
